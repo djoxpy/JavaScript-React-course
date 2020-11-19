a9daa2b1dfa70215d5fcf325e3bd9d25ce336d2f
OXPY <johnnybr7@gmail.com>
2020-11-19 02:11:54 +0200

    Second part, apply condition and cycles(unfinished)

--- firstApp/js/script.js
+++ firstApp/js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+let numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+
+for (let i = 0; numberOfFilms; i++) {
+    if (typeof numberOfFilms === 'string' || numberOfFilms.length < 50 || numberOfFilms) {
+        numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+    }
+}
+
+console.log(numberOfFilms);
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,16 +18,23 @@ let personalMovieDB = {
     privat: false,
 };
 
-let lastFilm;
-let filmRating; 
-
 for (let i = 0; i < 2; i++) {
-
-    lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-    filmRating = prompt("На сколько оцените его?", "");
-
+    let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    let filmRating = prompt("На сколько оцените его?", "");
+    
     personalMovieDB.movies[lastFilm] = filmRating;
 }
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (30 < personalMovieDB.count) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
 
 console.log(personalMovieDB);
 
